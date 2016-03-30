cef9c018a79e8dfc55486868fdedbad47ad8b245
Ondrej Zatloukal <ondrejzatloukal@Ondrejs-MacBook-Pro.local>
2016-03-30 10:16:01 -0700

    Added Moving Background

---
+++ Assets/Scripts/Background Scripts/BGLooper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private Vector2 offset = Vector2.zero;
+	private Material mat;
+
+
+	void Start () {
+		mat = GetComponent<Renderer> ().material; // Getting the Material
+		offset = mat.GetTextureOffset ("_MainTex");
+	}
+	
+
+	void Update () {
+		// Background Scrollig
+		offset.x += speed * Time.deltaTime;
+		mat.SetTextureOffset ("_MainTex", offset);
+	}
+}
---
+++ Assets/Scripts/Background Scripts/BGScaler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScaler : MonoBehaviour {
+
+	void Start () {
+
+		// Calculate the size of the Camera and fills the Quad with Background Material
+
+		var height = Camera.main.orthographicSize * 2f;
+		var width = height * Screen.width / Screen.height;
+
+		if (gameObject.name == "Background") {
+			transform.localScale = new Vector3 (width, height, 0);
+		} else { // Scale The Ground
+			transform.localScale = new Vector3 (width + 3f, 3, 0);
+		}
+	}
+}
